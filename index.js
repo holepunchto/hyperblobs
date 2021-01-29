4ef6802dc6a43cc6dc01c28353fb3b6622ac0f54
Andrew Osheroff <andrewosh@gmail.com>
2021-01-29 16:12:26 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const mutexify = require('mutexify')
+const { toPromises } = require('hypercore-promisifier')
+
+const { BlobReadStream, BlobWriteStream } = require('./lib/streams')
+
+const DEFAULT_BLOCK_SIZE = 2 ** 16
+
+module.exports = class Hyperblobs {
+  constructor (core, opts = {}) {
+    this.core = core
+    this.blockSize = opts.blockSize || DEFAULT_BLOCK_SIZE
+    this._lock = mutexify()
+    this._core = toPromises(core)
+  }
+
+  get feed () {
+    return this.core
+  }
+
+  async put (blob, opts) {
+    if (!Buffer.isBuffer(blob)) blob = Buffer.from(blob)
+    const blockSize = (opts && opts.blockSize) || this.blockSize
+
+    const stream = this.createWriteStream(opts)
+    for (let i = 0; i < blob.length; i += blockSize) {
+      stream.write(blob.slice(i, i + blockSize))
+    }
+    stream.end()
+
+    return new Promise((resolve, reject) => {
+      stream.once('error', reject)
+      stream.once('close', () => resolve(stream.id))
+    })
+  }
+
+  async get (id, opts) {
+    const res = []
+    for await (const block of this.createReadStream(id, opts)) {
+      res.push(block)
+    }
+    return Buffer.concat(res)
+  }
+
+  createReadStream (id, opts) {
+    return new BlobReadStream(this._core, id, opts)
+  }
+
+  createWriteStream (opts) {
+    return new BlobWriteStream(this._core, this._lock, opts)
+  }
+}
---
+++ test/all.js
@@ -0,0 +1,94 @@
+const test = require('tape')
+const hypercore = require('hypercore')
+const ram = require('random-access-memory')
+const { toPromises } = require('hypercore-promisifier')
+
+const Hyperblobs = require('..')
+
+test('can get/put a large blob', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core)
+
+  const buf = Buffer.alloc(5 * blobs.blockSize).fill('abcdefg')
+  const id = await blobs.put(buf)
+  const result = await blobs.get(id)
+  t.true(result.equals(buf))
+
+  t.end()
+})
+
+test('can put/get two blobs in one core', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core)
+
+  {
+    const buf = Buffer.alloc(5 * blobs.blockSize).fill('abcdefg')
+    const id = await blobs.put(buf)
+    const res = await blobs.get(id)
+    t.true(res.equals(buf))
+  }
+
+  {
+    const buf = Buffer.alloc(5 * blobs.blockSize).fill('hijklmn')
+    const id = await blobs.put(buf)
+    const res = await blobs.get(id)
+    t.true(res.equals(buf))
+  }
+
+  t.end()
+})
+
+test('can seek to start/length within one blob, one block', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core)
+
+  const buf = Buffer.alloc(5 * blobs.blockSize).fill('abcdefg')
+  const id = await blobs.put(buf)
+  const result = await blobs.get(id, { start: 2, length: 2 })
+  t.true(result.toString('utf-8'), 'cd')
+
+  t.end()
+})
+
+test('can seek to start/length within one blob, multiple blocks', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core, { blockSize: 10 })
+
+  const buf = Buffer.concat([Buffer.alloc(10).fill('a'), Buffer.alloc(10).fill('b')])
+  const id = await blobs.put(buf)
+  const result = await blobs.get(id, { start: 8, length: 4 })
+  t.true(result.toString('utf-8'), 'aabb')
+
+  t.end()
+})
+
+test('can seek to start/length within one blob, multiple blocks, multiple blobs', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core, { blockSize: 10 })
+
+  {
+    const buf = Buffer.alloc(5 * blobs.blockSize).fill('abcdefg')
+    const id = await blobs.put(buf)
+    const res = await blobs.get(id)
+    t.true(res.equals(buf))
+  }
+
+  const buf = Buffer.concat([Buffer.alloc(10).fill('a'), Buffer.alloc(10).fill('b')])
+  const id = await blobs.put(buf)
+  const result = await blobs.get(id, { start: 8, length: 4 })
+  t.true(result.toString('utf-8'), 'aabb')
+
+  t.end()
+})
+
+test('can seek to start/end within one blob', async t => {
+  const core = toPromises(hypercore(ram))
+  const blobs = new Hyperblobs(core)
+
+  const buf = Buffer.alloc(5 * blobs.blockSize).fill('abcdefg')
+  const id = await blobs.put(buf)
+  const result = await blobs.get(id, { start: 2, end: 4 }) // inclusive
+  t.true(result.toString('utf-8'), 'cd')
+
+  t.end()
+})
